81af9a29fe715f11cfde300edde02a1bc1098539
Tim <timyang@uw.edu>
2022-09-02 16:14:20 -0700

    posts fetch log

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -46,6 +46,7 @@ export default function Projects({ posts }: Props) {
 export const getServerSideProps = async () => {
   const query = `*[_type == 'post' && category[0]._ref in *[_type == 'category' && title == "Project"]._id]`
   const posts = await sanityClient.fetch(query)
+  console.log(posts)
   return {
     props: {
       posts,
