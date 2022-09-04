16a85013a56d6a2d039954841ddb9e8ce7132065
Tim <timyang@uw.edu>
2022-09-03 19:08:01 -0700

    gut projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -16,31 +16,31 @@ const Projects: NextPage<Props> = ({ posts }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 object-cover">
       {projectPosts.map((post, i) => (
-        <Link key={post._id} href={`/projects/${post.slug.current}`}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: i * 0.2 }}
-            className="border space-y-5 cursor-pointer h-full w-full hover:scale-105 ease-in-out transition-transform duration-200 p-5"
-          >
-            <Image
-              src={urlFor(post.mainImage).url()}
-              height="400"
-              width="640"
-              className="w-full h-2/3 object-scale-down"
-              sizes="(min-width: 75em) 33vw,
-              (min-width: 48em) 50vw,
-              100vw"
-              alt="post img"
-            />
-            <div className="p-5">
-              <h3 className="text-lg">{post.title}</h3>
-              <span className="text-xs" suppressHydrationWarning={true}>
-                {new Date(post._createdAt).toDateString()}
-              </span>
-            </div>
-          </motion.div>
-        </Link>
+        // <Link key={post._id} href={`/projects/${post.slug.current}`}>
+        //   <motion.div
+        //     initial={{ opacity: 0 }}
+        //     animate={{ opacity: 1 }}
+        //     transition={{ delay: i * 0.2 }}
+        //     className="border space-y-5 cursor-pointer h-full w-full hover:scale-105 ease-in-out transition-transform duration-200 p-5"
+        //   >
+        //     <Image
+        //       src={urlFor(post.mainImage).url()}
+        //       height="400"
+        //       width="640"
+        //       className="w-full h-2/3 object-scale-down"
+        //       sizes="(min-width: 75em) 33vw,
+        //       (min-width: 48em) 50vw,
+        //       100vw"
+        //       alt="post img"
+        //     />
+        //     <div className="p-5">
+        //       <h3 className="text-lg">{post.title}</h3>
+        //       <span className="text-xs" suppressHydrationWarning={true}>
+        //         {new Date(post._createdAt).toDateString()}
+        //       </span>
+        //     </div>
+        //   </motion.div>
+        // </Link>
       ))}
     </div>
   )
